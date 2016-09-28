13ffd1eb32e08bab84f159c69e20f875b14fdc5b
David Cisneros <d.cisneros849@gmail.com>
2016-09-28 02:09:47 -0700

    commented some code

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
+// Jquery for loading animation //
+
 $(window).on('load', function (e) {
-  setTimeout(loader, 3000)
+  setTimeout(loader, 3000);
   function loader() {
     $(".loader").fadeOut("slow");
   }
